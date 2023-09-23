0b3413357bf55e17f1edc932b6b8faf8ea6be9ea
Brijeshjivani0402 <brijesh20jivani@gmail.com>
2023-09-23 09:54:58 +0530

    Just i = edit and e = exit

--- main.c
+++ main.c
@@ -2,23 +2,43 @@
 
 /*Whole code in C*/
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include"header.h"
 
-void main()
+int main()
 {
+    char ch;
     int i=0;
     FILE *fp;
     char word[10];
 
-    fp=fopen("Text_Editor","w");
+    start:
+    system("clear");
+    printf("i = edit\n");
+    printf("e = exit\n");
 
-    for(i=0;i<10;i++)
+    ch = getchar();
+    if(ch=='i')
     {
-        fgets(word,10,stdin);
-        word[strlen(word)-1]='\0';
-        fprintf(fp,"%s\n",word);
-        fflush(fp);
+        system("clear");
+        fp=fopen("Text_Editor","w");
+        for(i=0;i<5;i++)
+        {
+            fgets(word,10,stdin);
+            word[strlen(word)-1]='\0';
+            fprintf(fp,"%s\n",word);
+            fflush(fp);
+        }
     }
+    else if(ch=='e')
+    {
+        system("clear");
+        return 0;
+    }
+    else
+    {
+        system("clear");
+        printf("Enter valid option\n");
+        goto start;
+    }      
+
 }
